78d0fd378632ba63a266f3ae4b53ec23730c7aaf
DWGichuru <49855521+DWGichuru@users.noreply.github.com>
2021-03-12 16:59:11 -0800

    site

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function About() {
+    return (
+        <Layout>
+            <div>
+                <h1>About Page</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit at imperdiet dui accumsan sit amet. Scelerisque viverra mauris in aliquam. Neque volutpat ac tincidunt vitae semper quis lectus. Adipiscing elit ut aliquam purus. Nibh mauris cursus mattis molestie a. Adipiscing bibendum est ultricies integer quis auctor. Neque volutpat ac tincidunt vitae semper quis. Sit amet tellus cras adipiscing enim eu. Maecenas pharetra convallis posuere morbi leo urna. Morbi enim nunc faucibus a.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit at imperdiet dui accumsan sit amet. Scelerisque viverra mauris in aliquam. Neque volutpat ac tincidunt vitae semper quis lectus. Adipiscing elit ut aliquam purus. Nibh mauris cursus mattis molestie a. Adipiscing bibendum est ultricies integer quis auctor. Neque volutpat ac tincidunt vitae semper quis. Sit amet tellus cras adipiscing enim eu. Maecenas pharetra convallis posuere morbi leo urna. Morbi enim nunc faucibus a.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit at imperdiet dui accumsan sit amet. Scelerisque viverra mauris in aliquam. Neque volutpat ac tincidunt vitae semper quis lectus. Adipiscing elit ut aliquam purus. Nibh mauris cursus mattis molestie a. Adipiscing bibendum est ultricies integer quis auctor. Neque volutpat ac tincidunt vitae semper quis. Sit amet tellus cras adipiscing enim eu. Maecenas pharetra convallis posuere morbi leo urna. Morbi enim nunc faucibus a.</p>
+            </div>
+        </Layout>
+    )
+}
+
